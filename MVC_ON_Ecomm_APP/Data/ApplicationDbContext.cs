8d3bae819d89bc9bf3ef4f112dcd4b50d482ede1
Bijjamvinay-1 <vinaybijjam7771@gmail.com>
2023-07-24 19:20:33 +0530

    here adding the data in category table to data base .by overiding the OnModelCreating method

--- MVC_ON_Ecomm_APP/Data/ApplicationDbContext.cs
+++ MVC_ON_Ecomm_APP/Data/ApplicationDbContext.cs
@@ -9,7 +9,19 @@ namespace MVC_ON_Ecomm_APP.Data
         {
 
         } 
-        public DbSet<Category>Categories { get; set; }  
-      
+        public DbSet<Category>Categories { get; set; }
+        // here adding the data in category table to data base .by overiding the OnModelCreating method
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Scifi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                  
+                );
+
+
+                
+        }
     }
 }
--- MVC_ON_Ecomm_APP/Migrations/20230724051403_Dummpy.Designer.cs
+++ MVC_ON_Ecomm_APP/Migrations/20230724051403_Dummpy.Designer.cs
@@ -3,6 +3,7 @@ using MVC_ON_Ecomm_APP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVC_ON_Ecomm_APP.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230724051403_Dummpy")]
+    partial class Dummpy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
